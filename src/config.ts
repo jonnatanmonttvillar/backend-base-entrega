b0b31570fe7292b5b5594a655b7081acaf465c82
Jonnatan Montt Villar <jonnatan_montt@hotmail.com>
2024-09-09 23:43:01 -0300

    update proyect

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const configuration = {
-    username: process.env.USERNAME ?? "Desconocido",
+    username: process.env.USERNAME ?? "COMO PASAR CONFIGMAP DE KUBERNETE AQUI",
     port: process.env.PORT ?? 3001,
-    apiKey: process.env.API_KEY ?? "Desconocida"
+    apiKey: process.env.API_KEY ?? "COMO PASAR CONFIGMAP DE KUBERNETE AQUI"
 }
